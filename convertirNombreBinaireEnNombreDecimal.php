93e9f9f30f31f5941934d0e3805cdf8bbd3c3728
naziradidier <nazirap20.aps1b@gmail.com>
2021-01-21 11:17:49 +0300

    Commit file

---
+++ convertirNombreBinaireEnNombreDecimal.php
@@ -0,0 +1,13 @@
+                               
+<?php
+// convertir le nombre decimal en nombre binaire
+$nbr1 = 292546;
+echo decbin($nbr1);
+ echo '</br>';
+
+ // convertir le nombre binaire en nombre decimal
+$nbr2 = 11111010111;
+echo bindec($nbr2);
+
+?>
+
